999b5778dd5cd818cb8af94bcbefd99ef646f1f0
Nicholas Tai <ntai@berkeley.edu>
2020-03-03 17:39:35 -0800

    added guide to connect gpages to react

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <ol>
+        <li>Create a new github repo with your username and githubio. Github pages specify what you need to name your repo in order to use ghpages</li>
+        <li>Download whatever it takes so you can run the command "npx create-react-app [insert ur name]"</li>
+        <li>use the command npx create-react-app</li>
+        <li>Now on github create ANOTHER repo and name it what you like. Once you created the repo on github youll see "…or push an existing repository from the command line"
+        Paste that command into your terminal while inside the react app folder you just created with npx command.</li>
+        <li>also use the command npm install --save ghpages</li>
+        <li>Now go into the package.json file and above dependencies paste in "homepage": "https://[insert ur user name].github.io/[insert the repo name]".
+        Note the repo name is not your githubio repo that you created it's the other one that you used to get the "...or push an existing repository..." command</li>
+        <li>Then add these two lines  "predeploy": "npm run build" AND "deploy": "gh-pages -d build" within the scripts in package.json. This should be below dependencies</li>
+        <li>Now use the command npm run deploy</li>
+        <li>Then go to the homepage link in your browser.</li>
+        <li>Also to check if you did it right, go to github and the repo you pushed the react app code onto. In the code tab, below commits, click on the branch and you should see ghpages. </li>
+      </ol>
       </header>
     </div>
   );
